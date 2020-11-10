b9996cb044f84ed1be91383462649c9d2dd72aeb
drenteria3 <72172845+drenteria3@users.noreply.github.com>
2020-11-10 01:54:08 -0600

    Add files via upload

---
+++ p1/ej/Ejercicio 2.js
@@ -0,0 +1,15 @@
+let x = 3;
+let k =[10];
+let c;
+for(c=0;c<11;c++)
+{
+    k[c]= factorial();
+}
+
+function factorial () {
+	var total = 1; 
+	for (i=1; i<=n; i++) {
+		total = total * i; 
+	}
+	return total; 
+}
---
+++ p1/ej/Ejercicio 4.js
@@ -0,0 +1,8 @@
+let x = 3;
+let k =[10];
+let c;
+for(c=0;c<11;c++)
+{
+    k[c]= Math.random(1,10);
+    document.write(k[c]);
+}
